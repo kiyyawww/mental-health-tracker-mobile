a3723ad858d0ed56d08f8139cb033dbc887287cf
kiyyawww <woydoy18@gmail.com>
2024-11-06 14:27:29 +0700

    menyelesaikan tutorial 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/menu.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.deepPurple,
+          primarySwatch: Colors.deepPurple,
         ).copyWith(secondary: Colors.deepPurple[400]),
         useMaterial3: true,
       ),
@@ -38,4 +38,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
